6fbfb91f2c8ffe5164c677fe9c9357da85fdd3c9
ZelenkovaEkaterina <54988537+ZelenkovaEkaterina@users.noreply.github.com>
2026-02-25 18:04:06 +0300

    Update EnemyHealthComponent.cs

--- Scripts/Enemy/EnemyHealthComponent.cs
+++ Scripts/Enemy/EnemyHealthComponent.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class EnemyHealthComponent : MonoBehaviour
 {
     public IDamageble _damagebleEnemy;
+    [SerializeField] private LayerMask _enemyLayerMask;
+    [SerializeField] private float _enemyDistance = .3f;
 
     private void Awake()
     {
         _damagebleEnemy = GetComponent<IDamageble>();
     }
 
-    private void Start(){
+    private void Start()
+    {
         if (_damagebleEnemy is DamageSystem handler)
         {
             handler.OnDamageTaken += (damage, source) =>
@@ -28,5 +31,40 @@ public class EnemyHealthComponent : MonoBehaviour
                 }
             };
         }
+    }
+
+    private void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.T) ||
+             Input.GetKeyDown(KeyCode.Y)) && IsPlayer())
+        {
+            //IsEnemy();
+            //_weapon.Attack(null);
+            //Debug.Log(null);
+            _damagebleEnemy?.TakeDamage(10, null);
+        }
+    }
+    
+    private bool IsPlayer()
+    {
+        RaycastHit2D hitRight = Physics2D.Raycast(
+            transform.position,
+            Vector2.right,
+            _enemyDistance,
+            _enemyLayerMask);
+        
+        RaycastHit2D hitLeft = Physics2D.Raycast(
+            transform.position,
+            Vector2.left,
+            _enemyDistance,
+            _enemyLayerMask);
+        //Debug.DrawLine(transform.position, hitLeft.point);
+        if (hitRight.collider || hitLeft.collider)
+        {
+            //Destroy(hit.collider.gameObject, .7f);
+            return true;
+        }
+        
+        return false;
     }
 }
